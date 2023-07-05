4b3c0980680971c2060334ea6b9ab5bb18e13e6e
Terence Hughes <Terencehughes450@gmail.com>
2023-07-05 13:52:54 -0400

    Finished lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+// Code your solutions in this files);
+const names = ["Charlie", "Samip", "Ali"];
+
+function writeCards(arrayNames) {
+    const newArray = []
+    for (let t = 0; t < arrayNames.length; t++) {
+        newArray.push(`Thank you, ${arrayNames[t]}, for the wonderful surprise gift!`);
+        debugger;
+}
+return newArray
+}
+
+function countDown(count) {
+    while (count >= 0){
+        console.log(count--);
+    }
+}
